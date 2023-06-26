d34410124484f4dddb964c8735a5e1d4f80fa446
iaIm14 <alexhuang1403@gmail.com>
2023-06-27 01:31:41 +0800

    feat: move MemTable to remote; add basic check.

--- memory/shared_package.cc
+++ memory/shared_package.cc
@@ -1,6 +1,7 @@
 #include "memory/shared_package.h"
 namespace shm_package {
 char *Pack(const std::string &raw) {
+  if (raw.empty()) return nullptr;
   char *ret = shm_alloc(raw.length());
   strncpy(ret, raw.data(), raw.length());
   return ret;
@@ -9,7 +10,7 @@ char *Pack(const std::string &raw) {
 void Unpack(void *u, std::string &t, size_t length) {
   // assert(t.empty());
   t.resize(length);
-  strncpy(t.data(), reinterpret_cast<char *>(u), length);
+  if (length) strncpy(t.data(), reinterpret_cast<char *>(u), length);
 }
 
 }  // namespace shm_package
